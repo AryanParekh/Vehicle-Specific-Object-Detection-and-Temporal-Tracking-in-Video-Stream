cacffccaa445a1c59ac1469450d4cf6b90515211
Aryan Parekh <aryanparekh2412@gmail.com>
2023-12-02 13:17:30 -0500

    minor change in training.py

--- training.py
+++ training.py
@@ -1,3 +1,4 @@
 from ultralytics import YOLO
+import os
 model = YOLO('yolov8n.yaml')
-model.train(data='data.yaml', epochs=100, batch=64, imgsz=640, optimizer='Adam')
+model.train(data=os.path.abspath("data.yaml"), epochs=100, batch=64, imgsz=640, optimizer='Adam')
